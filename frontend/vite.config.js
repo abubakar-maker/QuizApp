1fe1f76dee8bc651c56eb5d740a44954bcdf031a
abubakar <abubakaar9913@gmail.com>
2026-03-02 22:23:22 +0500

    Pussing on rende

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),],
+    preview: {
+    allowedHosts: true, // This allows Railway's domain to access the app
+  },
   server: {
     proxy: {
       // Proxy API requests to the backend to avoid CORS in local development
